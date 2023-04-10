3f5992d11eed4bce51bace8acb297917300bfce3
Yamil Buslemen <chamil.rbk@gmail.com>
2023-04-09 22:24:51 -0300

    proyecto javascript coderhouse

---
+++ data/stock.js
@@ -0,0 +1,68 @@
+const productos =
+[
+    {
+        id: 1,
+        nombre: "Guantes Boxeo",
+        tipo: "Guantes",
+        desc: "Guantes 12/14 oz",
+        precio: 12000,
+        talle: "L",
+        imagen: './public/images/Guantes.webp',
+        cantidad:1
+    },
+
+    {
+        id: 2,
+        nombre: "Vendas 3mts",
+        tipo: "Vendas",
+        desc: "Vendas protectoras",
+        precio: 3000,
+        talle: "L",
+        imagen: './public/images/vendas.webp',
+        cantidad:1
+    },
+
+    {
+        id: 3,
+        nombre: "Protector bucal",
+        tipo: "Protector",
+        desc: "Protector bucal",
+        precio: 2000,
+        talle: "S",
+        imagen: './public/images/bucal.webp',
+        cantidad:1
+    },
+
+    {
+        id: 4,
+        nombre: "Proteinas",
+        tipo: "Proteinas",
+        desc: "Suplemento nutricional a base de proteinas animales",
+        precio: 16000,
+        talle: "L",
+        imagen: './public/images/proteinas.webp',
+        cantidad:1
+    },
+
+    {
+        id: 5,
+        nombre: "Shaker",
+        tipo: "Shaker",
+        desc: "Vaso batidor para tus proteinas",
+        precio: 5000,
+        talle: "L",
+        imagen: './public/images/shaker.webp',
+        cantidad:1
+    },
+
+    {
+        id: 6,
+        nombre: "Cinturon",
+        tipo: "Cinturon de fuerza",
+        desc: "Cinturon de fuerza para reforzar tus levantamientos",
+        precio: 10000,
+        talle: "L",
+        imagen: './public/images/cinturon.webp',
+        cantidad:1
+    }
+];
